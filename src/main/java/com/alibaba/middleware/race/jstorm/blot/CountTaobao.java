88674088f8ba2680b66994f2436291ba81cd7a12
sumBug <sxian.wang@gmail.com>
2016-05-31 19:07:20 +0800

    add declarer field of count taobao

--- src/main/java/com/alibaba/middleware/race/jstorm/blot/CountTaobao.java
+++ src/main/java/com/alibaba/middleware/race/jstorm/blot/CountTaobao.java
@@ -4,6 +4,7 @@ import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.IRichBolt;
 import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 import com.alibaba.middleware.race.RaceUtils;
@@ -59,7 +60,7 @@ public class CountTaobao implements IRichBolt, Serializable {
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
-
+        declarer.declare(new Fields("countTaobao"));
     }
 
     @Override
