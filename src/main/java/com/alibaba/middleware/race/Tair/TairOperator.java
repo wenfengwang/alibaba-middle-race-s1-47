68b3eb7be4b696b85a0b79d99310aa90849476aa
sxian.wang <sxian.wang@gmail.com>
2016-06-29 17:56:21 +0800

    complete tair

--- src/main/java/com/alibaba/middleware/race/Tair/TairOperator.java
+++ src/main/java/com/alibaba/middleware/race/Tair/TairOperator.java
@@ -1,5 +1,7 @@
 package com.alibaba.middleware.race.Tair;
 
+import com.alibaba.middleware.race.RaceConfig;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -10,7 +12,10 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class TairOperator {
     public static void main(String[] args) {
-        String str = "01234567891011";
-        System.out.println(str.substring(1));
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("192.168.1.161:5198");
+        TairOperatorImpl tairOperator = new TairOperatorImpl(list);
+//        tairOperator.write("asd","123");
+        System.out.println(tairOperator.get(RaceConfig.prex_taobao+"1467191460"));
     }
 }
