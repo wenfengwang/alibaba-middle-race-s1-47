53ee88733ef9f977558229d19289a11ead33e1e2
sxian.wang <sxian.wang@gmail.com>
2016-07-03 19:47:36 +0800

    fix bug

--- src/main/java/com/alibaba/middleware/race/RaceConfig.java
+++ src/main/java/com/alibaba/middleware/race/RaceConfig.java
@@ -25,7 +25,7 @@ public class RaceConfig implements Serializable {
     public static final String prex_ratio = "ratio_" + TeamCode + "_";
 
     // online
-    public static final int Namespace = 19542;
+    public static final int TairNamespace = 19542;
     public static final List<String> TairServerAddr = Arrays.asList(new String[]{"10.101.72.127:5198","10.101.72.129:5198"});
     // offline
 //    public static final List<String> TairServerAddr = Arrays.asList(new String[]{"192.168.1.161:5198"});
