bcbd0365702fde53ee99d80b50d952037223a0f3
sxian.wang <sxian.wang@gmail.com>
2016-07-06 19:48:11 +0800

    123

--- src/main/java/com/alibaba/middleware/race/RaceConfig.java
+++ src/main/java/com/alibaba/middleware/race/RaceConfig.java
@@ -37,7 +37,7 @@ public class RaceConfig implements Serializable {
     public static final String TB_LOG_PATH = "E:\\mdw_log\\Tb_";
     public static final String PY_LOG_PATH = "E:\\mdw_log\\Py_";
 
-    public static final String CREATE_DATA_PREFIX = "E:\\mdw_data\\";
+    public static final String CREATE_DATA_PREFIX = "E:\\mdw_data\\30min\\";
     public static final String FILE_PRODUCER_SOURCE_PREFIX = "E:\\mdw_data\\";
 
 //    public static final String TB_LOG_PATH = "/home/dev/mdw_log/Tb_";
